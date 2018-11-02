2fb1b5422621ad81c7530bc5f8181b8cdbf427b4
nicogaviria7 <37244302+nicogaviria7@users.noreply.github.com>
2018-11-02 15:37:25 -0500

    Add files via upload
    Adición-Tipología

--- GRI_Cliente/js/data.js
+++ GRI_Cliente/js/data.js
@@ -3,45 +3,23 @@
 
   $(document).ready( function () {
 
-var id = sessionStorage.getItem('id');
-console.log(id);
-  // Toggle the side navigation
-  $("#sidebarToggle").on('click',function(e) {
-    e.preventDefault();
-    $("body").toggleClass("sidebar-toggled");
-    $(".sidebar").toggleClass("toggled");
-  });
+    var data = JSON.parse(sessionStorage.getItem('data'));
+    var type= sessionStorage.getItem('type');
 
-  // Prevent the content wrapper from scrolling when the fixed side navigation hovered over
-  $('body.fixed-nav .sidebar').on('mousewheel DOMMouseScroll wheel', function(e) {
-    if ($(window).width() > 768) {
-      var e0 = e.originalEvent,
-      delta = e0.wheelDelta || -e0.detail;
-      this.scrollTop += (delta < 0 ? 1 : -1) * 30;
-      e.preventDefault();
+    document.getElementById('titulo').innerHTML = data.nombre;
+
+    if(type=='i'){
+      document.getElementById('side-investigadores').classList.add('active');
+    }else if(type=='g'){
+      document.getElementById('side-grupos').classList.add('active');
+    }else if(type=='f'){
+      document.getElementById('side-facultades').classList.add('active');
+    }else if(type=='p'){
+      document.getElementById('side-programas').classList.add('active');
+    }else if(type=='c'){
+      document.getElementById('side-centros').classList.add('active');
+    }else{
+      document.getElementById('side-inicio').classList.add('active');
     }
-  });
-
-  // Scroll to top button appear
-  $(document).on('scroll',function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
-
-  // Smooth scrolling using jQuery easing
-  $(document).on('click', 'a.scroll-to-top', function(event) {
-    var $anchor = $(this);
-    $('html, body').stop().animate({
-      scrollTop: ($($anchor.attr('href')).offset().top)
-    }, 1000, 'easeInOutExpo');
-    event.preventDefault();
-  });
-
-
-
   } );
 })(jQuery); // End of use strict
--- GRI_Cliente/js/gri.js
+++ GRI_Cliente/js/gri.js
@@ -2,6 +2,7 @@
   "use strict"; // Start of use strict
 
   $(document).ready( function () {
+    
   // Toggle the side navigation
   $("#sidebarToggle").on('click',function(e) {
     e.preventDefault();
@@ -38,7 +39,7 @@
     event.preventDefault();
   });
 
-  var table= $('#table_id').DataTable( {
+  var tabla_investigadores= $('#tabla_investigadores').DataTable( {
       responsive: true,
       ajax:{
         url:'http://localhost:8080/Test/rest/empservice/emps',
@@ -92,12 +93,261 @@
         ]
       } );
 
-   $('#table_id tbody').on('click', 'tr', function () {
-        var data = table.row( this ).data();
-        sessionStorage.setItem('id', data.id);
+  var tabla_grupos= $('#tabla_grupos').DataTable( {
+      responsive: true,
+      ajax:{
+        url:'http://localhost:8080/Test/rest/empservice/emps',
+        dataSrc:"" },
+        rowId: 'id',
+        dom: 'Bfrtip',
+        buttons: [
+        {
+          extend: 'copy',
+          text: 'Copiar',
+          className: 'copyButton'
+        }, 
+        {
+          extend: 'excel',
+          text: 'Excel',
+          className: 'excelButton'
+        },  
+        {
+          extend: 'pdf',
+          text: 'PDF',
+          className: 'pdfButton'
+        },
+        {
+          extend: 'print',
+          text: 'Imprimir',
+          className: 'printButton'
+        }
+        ],
+
+        language: {
+        processing:     "Procesamiento en curso...",
+        search:         "Buscar: ",
+        lengthMenu:    "Mostrando _MENU_ elementos",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ elementos",
+        infoEmpty:      "Mostrando 0 a 0 de 0 elementos",
+        infoFiltered:   "(filtrado de _MAX_ elementos en total)",
+        infoPostFix:    "",
+        loadingRecords: "Cargando resultados...",
+        zeroRecords:    "No hay información para mostrar",
+        emptyTable:     "No hay información para mostrar",
+        paginate: {
+            first:      "Primera",
+            previous:   "Anterior",
+            next:       "Siguiente",
+            last:       "última"
+        }
+      },
+        columns: [
+        { data: "id" },
+        { data: "nombre" }          
+        ]
+      } );
+
+  var tabla_facultades= $('#tabla_facultades').DataTable( {
+      responsive: true,
+      ajax:{
+        url:'http://localhost:8080/Test/rest/empservice/emps',
+        dataSrc:"" },
+        rowId: 'id',
+        dom: 'Bfrtip',
+        buttons: [
+        {
+          extend: 'copy',
+          text: 'Copiar',
+          className: 'copyButton'
+        }, 
+        {
+          extend: 'excel',
+          text: 'Excel',
+          className: 'excelButton'
+        },  
+        {
+          extend: 'pdf',
+          text: 'PDF',
+          className: 'pdfButton'
+        },
+        {
+          extend: 'print',
+          text: 'Imprimir',
+          className: 'printButton'
+        }
+        ],
+
+        language: {
+        processing:     "Procesamiento en curso...",
+        search:         "Buscar: ",
+        lengthMenu:    "Mostrando _MENU_ elementos",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ elementos",
+        infoEmpty:      "Mostrando 0 a 0 de 0 elementos",
+        infoFiltered:   "(filtrado de _MAX_ elementos en total)",
+        infoPostFix:    "",
+        loadingRecords: "Cargando resultados...",
+        zeroRecords:    "No hay información para mostrar",
+        emptyTable:     "No hay información para mostrar",
+        paginate: {
+            first:      "Primera",
+            previous:   "Anterior",
+            next:       "Siguiente",
+            last:       "última"
+        }
+      },
+        columns: [
+        { data: "id" },
+        { data: "nombre" }          
+        ]
+      } );
+
+  var tabla_programas= $('#tabla_programas').DataTable( {
+      responsive: true,
+      ajax:{
+        url:'http://localhost:8080/Test/rest/empservice/emps',
+        dataSrc:"" },
+        rowId: 'id',
+        dom: 'Bfrtip',
+        buttons: [
+        {
+          extend: 'copy',
+          text: 'Copiar',
+          className: 'copyButton'
+        }, 
+        {
+          extend: 'excel',
+          text: 'Excel',
+          className: 'excelButton'
+        },  
+        {
+          extend: 'pdf',
+          text: 'PDF',
+          className: 'pdfButton'
+        },
+        {
+          extend: 'print',
+          text: 'Imprimir',
+          className: 'printButton'
+        }
+        ],
+
+        language: {
+        processing:     "Procesamiento en curso...",
+        search:         "Buscar: ",
+        lengthMenu:    "Mostrando _MENU_ elementos",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ elementos",
+        infoEmpty:      "Mostrando 0 a 0 de 0 elementos",
+        infoFiltered:   "(filtrado de _MAX_ elementos en total)",
+        infoPostFix:    "",
+        loadingRecords: "Cargando resultados...",
+        zeroRecords:    "No hay información para mostrar",
+        emptyTable:     "No hay información para mostrar",
+        paginate: {
+            first:      "Primera",
+            previous:   "Anterior",
+            next:       "Siguiente",
+            last:       "última"
+        }
+      },
+        columns: [
+        { data: "id" },
+        { data: "nombre" }          
+        ]
+      } );
+
+  var tabla_centros= $('#tabla_centros').DataTable( {
+      responsive: true,
+      ajax:{
+        url:'http://localhost:8080/Test/rest/empservice/emps',
+        dataSrc:"" },
+        rowId: 'id',
+        dom: 'Bfrtip',
+        buttons: [
+        {
+          extend: 'copy',
+          text: 'Copiar',
+          className: 'copyButton'
+        }, 
+        {
+          extend: 'excel',
+          text: 'Excel',
+          className: 'excelButton'
+        },  
+        {
+          extend: 'pdf',
+          text: 'PDF',
+          className: 'pdfButton'
+        },
+        {
+          extend: 'print',
+          text: 'Imprimir',
+          className: 'printButton'
+        }
+        ],
+
+        language: {
+        processing:     "Procesamiento en curso...",
+        search:         "Buscar: ",
+        lengthMenu:    "Mostrando _MENU_ elementos",
+        info:           "Mostrando _START_ a _END_ de _TOTAL_ elementos",
+        infoEmpty:      "Mostrando 0 a 0 de 0 elementos",
+        infoFiltered:   "(filtrado de _MAX_ elementos en total)",
+        infoPostFix:    "",
+        loadingRecords: "Cargando resultados...",
+        zeroRecords:    "No hay información para mostrar",
+        emptyTable:     "No hay información para mostrar",
+        paginate: {
+            first:      "Primera",
+            previous:   "Anterior",
+            next:       "Siguiente",
+            last:       "última"
+        }
+      },
+        columns: [
+        { data: "id" },
+        { data: "nombre" }          
+        ]
+      } );
+
+   $('#tabla_investigadores tbody').on('click', 'tr', function () {
+        var data = tabla_investigadores.row( this ).data();
+
+        sessionStorage.setItem('data', JSON.stringify(data));
+        sessionStorage.setItem('type', 'i');
         window.location.replace("home.html");
     } );
 
+    $('#tabla_grupos tbody').on('click', 'tr', function () {
+        var data = tabla_grupos.row( this ).data();
+
+        sessionStorage.setItem('data', JSON.stringify(data));
+        sessionStorage.setItem('type', 'g');
+        window.location.replace("home.html");
+    } );
+
+     $('#tabla_facultades tbody').on('click', 'tr', function () {
+        var data = tabla_facultades.row( this ).data();
+
+        sessionStorage.setItem('data', JSON.stringify(data));
+        sessionStorage.setItem('type', 'f');
+        window.location.replace("home.html");
+    } );
+
+      $('#tabla_programas tbody').on('click', 'tr', function () {
+        var data = tabla_programas.row( this ).data();
+
+        sessionStorage.setItem('data', JSON.stringify(data));
+        sessionStorage.setItem('type', 'p');
+        window.location.replace("home.html");
+    } );
+
+       $('#tabla_centros tbody').on('click', 'tr', function () {
+        var data = tabla_centros.row( this ).data();
+
+        sessionStorage.setItem('data', JSON.stringify(data));
+        sessionStorage.setItem('type', 'c');
+        window.location.replace("home.html");
+    } );
 
 
   } );
